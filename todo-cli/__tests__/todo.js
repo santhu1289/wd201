1d0145a3256114e130e932e72509054c578437e2
Santhosh Devale <santhosh.rao@egovernments.org>
2024-10-07 23:00:08 +0530

    updating the varoius packages

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,22 +1,22 @@
-const { default: test } = require("node:test");
-const todoList = require("../todo");
+const { default: test } = require('node:test')
+const todoList = require('../todo')
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add } = todoList()
 
-describe("TodoList Test Suits", () => {
-  test("Should add new todo", () => {
-    expect(all.length).toBe(0);
+describe('TodoList Test Suits', () => {
+  test('Should add new todo', () => {
+    expect(all.length).toBe(0)
     add({
-      title: "Test todo",
+      title: 'Test todo',
       completed: false,
-      dueDate: new Date().toISOString("en-CA"),
-    });
-    expect(all.length).toBe(1);
-  });
+      dueDate: new Date().toISOString('en-CA'),
+    })
+    expect(all.length).toBe(1)
+  })
 
-  test("should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-});
+  test('should mark a todo as complete', () => {
+    expect(all[0].completed).toBe(false)
+    markAsComplete(0)
+    expect(all[0].completed).toBe(true)
+  })
+})
