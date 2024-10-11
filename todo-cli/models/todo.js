58a015704dd04d8d73dbf6aafedc58ccfc15e5ca
Santhosh Devale <santhosh.rao@egovernments.org>
2024-10-11 15:58:07 +0530

    todo file code updated

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,102 +1,99 @@
 // models/todo.js
 'use strict'
-const { Op } = require('sequelize')
-const { Model } = require('sequelize')
+const { Model, Op } = require('sequelize')
+const moment = require('moment')
 
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+
+    // Add a new task to the todo list
     static async addTask(params) {
-      return await Todo.create({
-        ...params,
-        completed: params.completed || false, // Default to false if not provided
-      })
+      return await Todo.create(params)
     }
 
+    // Show the entire list in the required format
     static async showList() {
-      console.log('My Todo list \n')
+      console.log('My Todo-list\n')
 
       console.log('Overdue')
-      const overdueTasks = await this.overdue()
+      const overdueTasks = await Todo.overdue()
       overdueTasks.forEach((task) => console.log(task.displayableString()))
       console.log('\n')
 
       console.log('Due Today')
-      const todayTasks = await this.dueToday()
+      const todayTasks = await Todo.dueToday()
       todayTasks.forEach((task) => console.log(task.displayableString()))
       console.log('\n')
 
       console.log('Due Later')
-      const laterTasks = await this.dueLater()
+      const laterTasks = await Todo.dueLater()
       laterTasks.forEach((task) => console.log(task.displayableString()))
     }
 
+    // Get overdue tasks (tasks whose due date is before today and are not completed)
     static async overdue() {
-      const today = new Date().toISOString().split('T')[0]
+      const today = moment().format('YYYY-MM-DD')
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.lt]: today },
-          completed: true, // Change to true to fetch completed tasks
-        },
-      })
-    }
-
-    static async dueToday() {
-      const today = new Date().toISOString().split('T')[0]
-      return await Todo.findAll({
-        where: {
-          dueDate: today,
+          dueDate: {
+            [Op.lt]: today,
+          },
           completed: false,
         },
       })
     }
 
-    static async dueLater() {
-      const today = new Date().toISOString().split('T')[0]
+    // Get tasks that are due today
+    static async dueToday() {
+      const today = moment().format('YYYY-MM-DD')
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.gt]: today },
-          completed: true,
+          dueDate: today,
         },
       })
     }
 
+    // Get tasks that are due later (tasks whose due date is after today)
+    static async dueLater() {
+      const today = moment().format('YYYY-MM-DD')
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: today,
+          },
+        },
+      })
+    }
+
+    // Mark a task as completed
     static async markAsComplete(id) {
-      const todo = await Todo.findByPk(id)
-      if (todo) {
-        console.log('Before marking complete:', todo)
-        todo.completed = true
-        await todo.save()
-        console.log('After marking complete:', todo)
+      const task = await Todo.findByPk(id)
+      if (task) {
+        task.completed = true
+        await task.save()
       }
     }
 
+    // Convert task into a displayable string
     displayableString() {
       const checkbox = this.completed ? '[x]' : '[ ]'
-      const today = new Date().toISOString().split('T')[0]
-
-      // Always include the due date for completed past-due tasks
-      if (this.completed) {
-        return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`
-      }
-
-      // If the task is due today, do not display the dueDate
-      if (this.dueDate === today) {
-        return `${this.id}. ${checkbox} ${this.title}`
-      }
-
-      // For overdue and future tasks, display the date
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`
+      const formattedDate =
+        this.dueDate === moment().format('YYYY-MM-DD') ? '' : this.dueDate
+      return `${this.id}. ${checkbox} ${this.title.trim()} ${formattedDate}`
     }
   }
 
+  // Define the model's schema
   Todo.init(
     {
       title: DataTypes.STRING,
       dueDate: DataTypes.DATEONLY,
-      completed: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false, // Set default value to false
-      },
+      completed: DataTypes.BOOLEAN,
     },
     {
       sequelize,
