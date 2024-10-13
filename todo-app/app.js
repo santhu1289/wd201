8df08b05451ba6ba48489124db934b27914c503b
Santhosh Devale <santhosh.rao@egovernments.org>
2024-10-13 14:20:38 +0530

    initial upload todo-app

---
+++ todo-app/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+//app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/todos", (request, response) => {
+  //response.send("hello World");
+  console.log("Todo Lsit");
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("creating a Todo", request.body);
+  //Todo
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to updateto todo with ID: ", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updateTodo = await todo.markAsCompleted();
+    return response.json(updateTodo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", (request, response) => {
+  console.log("Delete a todo by ID: ", request.params.id);
+});
+
+module.exports = app;
