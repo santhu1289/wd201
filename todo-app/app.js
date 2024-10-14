acdb2e5c64a828579d443b39a6ae7b531a9c658f
Santhosh Devale <santhosh.rao@egovernments.org>
2024-10-14 16:29:23 +0530

    Prod Script Upgrade

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", {
+      allTodos,
+    });
+  } else {
+    response.json("index", {
+      allTodos,
+    });
+  }
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET /todos - Fetch all To-Dos from the database
 app.get("/todos", async function (_request, response) {
